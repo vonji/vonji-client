b6f9d8ecaabc931c050a3318f718472c1a158b8d
Loup Peluso <loup.peluso@gmail.com>
2016-07-01 00:18:35 +0200

    Refactorized the Request Add/Edit views
    * HTML template factorized in a stateless component.
    *  Improved typo and naming clarity (removed Request in favor of a plain object with title and content, data is fully designed from the beginning so we know how it should be setup by actions).

--- src/components/requests/RequestAdd.vue
+++ src/components/requests/RequestAdd.vue
@@ -1,55 +1,27 @@
 <template xmlns:v-on="http://www.w3.org/1999/xhtml">
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-      <div class="form">
-        <div class="form-group">
-          <label for="#request-title">Request title</label>
-          <input
-            type="text" id="request-title"
-            class="form-control"
-            v-model="request.Title"
-            placeholder="Title of your request..."
-          />
-        </div>
-        <div class="form-group">
-          <label for="#request-content">Description of your task</label>
-          <textarea
-            id="request-content"
-            rows="10"
-            class="form-control"
-            v-model="request.Content"
-            placeholder="Describe your request..."
-          ></textarea>
-        </div>
-        <button @click="save" class="btn btn-primary btn-block btn-lg">Post request</button>
-      </div>
-    </div>
-  </div>
+  <request-edit-partial
+    :title="Title"
+    :content="Content"
+  ></request-edit-partial>
 </template>
 
 <script type="text/babel">
+import Resources from '../../utils/resources'
 
-    import Resources from '../../utils/resources'
-
-    export default {
-        data() {
-            return {
-                request: {}
-            };
-        },
-        methods: {
-            save: function () {
-                Resources.request.post({
-                    data: JSON.stringify(this.request),
-                    redirect: '/requests'
-                });
-            }
-        }
-    }
+export default {
+  data() {
+    return {
+      Title: '',
+      Content: '',
+    };
+  },
+  events: {
+    'on-save': function (Title, Content) {
+      Resources.request.post({
+        data: JSON.stringify({ Title, Content }),
+        redirect: '/requests'
+      });
+    },
+  },
+}
 </script>
-
-<style>
-  textarea {
-    resize: vertical;
-  }
-</style>
---
+++ src/components/requests/RequestEditPartial.vue
@@ -0,0 +1,45 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="form">
+        <div class="form-group">
+          <label for="#request-title">Request title</label>
+          <input
+            type="text" id="request-title"
+            v-model="title"
+            class="form-control"
+            placeholder="Title of your request..."
+          />
+        </div>
+        <div class="form-group">
+          <label for="#request-content">Description of your task</label>
+          <textarea
+            id="request-content"
+            rows="10"
+            v-model="content"
+            class="form-control"
+            placeholder="Describe your request..."
+          ></textarea>
+        </div>
+        <button @click="save" class="btn btn-primary btn-block btn-lg">Save</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/babel">
+export default {
+  props: ['title', 'content', 'id'],
+  methods: {
+    save() {
+      this.$dispatch('on-save', this.title, this.content, this.id);
+    }
+  }
+}
+</script>
+
+<style>
+textarea {
+  resize: vertical;
+}
+</style>
