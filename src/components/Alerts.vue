48597e7613a7c89f513175e357ede39ff32a6a68
mantal <mantal0@outlook.fr>
2016-07-01 11:31:07 +0200

    Add alerts

---
+++ src/components/Alerts.vue
@@ -0,0 +1,29 @@
+<template>
+    <div> <!-- single root level so the component is not a fragment -->
+        <template v-for="alert in alerts">
+            <div @click="alertDismiss($index)" class="alert alert-{{alert.type}}">
+                {{alert.message}}
+            </div>
+        </template>
+    </div>
+</template>
+
+<script type="text/babel">
+
+    import { alertDismiss } from '../vuex/actions'
+
+    export default {
+        date() {
+            return {
+            }
+        },
+        vuex: {
+            getters: {
+                alerts: (state) => state.alerts
+            },
+            actions: {
+                alertDismiss
+            }
+        }
+    }
+</script>
--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -4,7 +4,10 @@ export const login = ({dispatch}, email, password) => {
     Vue.http.get('http://localhost:1618/users/' + email).then((result) => {
         "use strict";
         if (JSON.parse(result.data).Password == password)
+        {
             dispatch('LOGIN', email, password);
+            dispatch('ALERT', 'info', 'Logged in');
+        }
         else
             console.error('Bad password: ' + password);
     });
@@ -12,4 +15,15 @@ export const login = ({dispatch}, email, password) => {
 
 export const logout = ({dispatch}) => {
     dispatch('LOGOUT');
+    dispatch('ALERT', 'info', 'Logged out');
+};
+
+export const alert = ({dispatch}, type, message) => {
+    "use strict";
+    dispatch('ALERT');
+};
+
+export const alertDismiss = ({dispatch}, index) => {
+    "use strict";
+    dispatch('ALERT_DISMISS', index);
 };
