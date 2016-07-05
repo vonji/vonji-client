07befcefc090b7f85ac40f4e897d850dd3b21dc8
mantal <mantal0@outlook.fr>
2016-07-05 12:12:49 +0200

    Use html form in login

--- src/components/topbar/LoginForm.vue
+++ src/components/topbar/LoginForm.vue
@@ -1,39 +1,41 @@
 <template>
 	<div class="navbar-form navbar-right">
-		<div class="form-group">
-			<input v-model="email" type="email" class="form-control" placeholder="Login" />
-		</div>
-		<div class="form-group">
-			<input v-model="password" type="password" class="form-control" placeholder="Password" />
-		</div>
-		<button type="submit" @click="submit" class="btn {{ isDisabled ? 'btn-default' : 'btn-primary' }}" disabled="{{ isDisabled }}">
-			Login
-		</button>
-		<a class="btn btn-primary" v-link="'/users/add'">Sign in</a>
+		<form @submit.prevent="submit" role="form" autocomplete="on" class="form-inline">
+			<div class="form-group">
+				<input v-model="email" type="email" class="form-control" placeholder="Login"/>
+			</div>
+			<div class="form-group">
+				<input v-model="password" type="password" class="form-control" placeholder="Password"/>
+			</div>
+			<button type="submit" class="btn {{ isDisabled ? 'btn-default' : 'btn-primary' }}" disabled="{{ isDisabled }}">
+				Login
+			</button>
+			<a class="btn btn-primary" v-link="'/users/add'">Sign in</a>
+		</form>
 	</div>
 </template>
 
 <script>
-import {login} from '../../vuex/actions';
+	import { login } from '../../vuex/actions';
 
-export default {
-	data() {
-		return {
-			email: '',
-			password: ''
-		};
-	},
-	computed: {
-		isDisabled() {
-			return this.email === '' || this.password === '';
-		}
-	},
-	vuex: {
-		actions: {
-			submit({dispatch}, e) {
-				login({dispatch}, this.email, this.password);
+	export default {
+		data() {
+			return {
+				email: '',
+				password: ''
+			};
+		},
+		computed: {
+			isDisabled() {
+				return this.email === '' || this.password === '';
+			}
+		},
+		vuex: {
+			actions: {
+				submit({ dispatch }, e) {
+					login({ dispatch }, this.email, this.password);
+				}
 			}
 		}
 	}
-}
 </script>
