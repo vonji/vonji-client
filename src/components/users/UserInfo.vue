8bd0ac1e04e07f4c2f94e288c63721a85fcefde9
mantal <mantal0@outlook.fr>
2016-07-09 12:00:38 +0200

    Add Request

---
+++ src/components/users/UserInfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="user-info">
+        <div>Responded 2s ago</div>
+        <img src="https://placehold.it/50x50" alt="avatar">
+        {{user.FirstName}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        props: ['user']
+    }
+</script>
+
+<style>
+    .user-info {
+        background-color: blanchedalmond;
+        height: 100px;
+    }
+</style>
