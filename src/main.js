50b8bc187e13a44d123b3f5a75acf1ca75a903e3
mantal <mantal0@outlook.fr>
2016-07-09 11:58:10 +0200

    Setup Vue resource and Vue router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
+import Router from 'vue-router';
+import Resource from 'vue-resource'
 import App from './components/App.vue';
 
+import Navbar from './components/Navbar.vue'
 global.jQuery = require('jquery');
 require('bootstrap-loader');
 
-new Vue({
-  el: 'body',
-  components: {
-    app: App,
-  },
+Vue.use(Router);
+Vue.use(Resource);
+
+Vue.http.options.root = 'http://localhost:1618/';
+
+let router = new Router();
+
+router.map({
 });
+
+router.start(App, '#app');
