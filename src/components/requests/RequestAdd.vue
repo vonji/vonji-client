bcd78cfa3567b55b29bf1b163191ff7e4e7fdfc1
mantal <mantal0@outlook.fr>
2016-07-09 12:08:03 +0200

    Add Request add

---
+++ src/components/requests/RequestAdd.vue
@@ -0,0 +1,36 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+    <div class="row">
+        <div class="col-md-12">
+            <input type="text" title="title" v-model="request.Title">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <textarea title="content" v-model="request.Content"></textarea>
+        </div>
+    </div>
+    <button v-on:click="save">Post request</button>
+</template>
+
+<script type="text/babel">
+
+    import vonji from '../../utils/ajax'
+
+    export default {
+        data() {
+            return {
+                request: {}
+            };
+        },
+        methods: {
+            save: function () {
+                vonji.post({
+                    url: 'requests',
+                    data: JSON.stringify(this.request),
+                    //redirect: 'requests/request.html?id=' + id
+                });
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import UserAdd from './components/users/UserAdd.vue'
 import UserList from './components/users/UserList.vue'
 
 import Request from './components/requests/Request.vue'
+import RequestAdd from './components/requests/RequestAdd.vue'
 import RequestEdit from './components/requests/RequestEdit.vue'
 import RequestList from './components/requests/RequestList.vue'
 import ResponseEdit from './components/requests/ResponseEdit.vue'
@@ -37,8 +38,12 @@ router.map({
     '/requests': {
         component: RequestList
     },
+    '/requests/add/': {
+        component: RequestAdd
+    },
     '/requests/edit/:id': {
         component: RequestEdit
+    },
     '/requests/view/:id': {
         component: Request
     },
