6aef76dabae7e8c8cde0809fb6e00daad7cc41bf
mantal <mantal0@outlook.fr>
2016-08-27 13:11:53 +0200

    Improve search bar look

--- src/components/bootstrap/BsSearchBar.vue
+++ src/components/bootstrap/BsSearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-	<form @submit.prevent="foo()" class="form-group {{ inline ? 'form-inline' : '' }}">
+	<form @submit.prevent="foo()" class="{{ inline ? 'form-inline' : '' }}">
 		<div class="input-group">
 			<span class="input-group-btn"><button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" style="line-height: inherit"></span></button></span>
 			<input type="search" v-model="model" class="form-control" placeholder="{{placeholder}}">
@@ -30,3 +30,9 @@
 		}
 	}
 </script>
+
+<style scoped>
+	form {
+		margin-bottom: 0px!important;
+	}
+</style>
