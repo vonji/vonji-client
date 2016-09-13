3508f57185d8e9a818df5addf5b1005a7e025bd8
mantal <mantal0@outlook.fr>
2016-09-13 12:06:59 +0200

    VON-95 Topbar display vAction instead of vCoins

--- src/components/topbar/Topbar.vue
+++ src/components/topbar/Topbar.vue
@@ -9,34 +9,34 @@
 		<login-form v-if="isGuest" class="nav navbar-nav navbar-right"></login-form>
 		<ul v-else class="nav navbar-nav navbar-right">
 			<li><img :src="currentUser.Avatar" alt="user-avatar" width="40" height="40" class="hidden-xs topbar-avatar img-rounded"></li>
-			<li><a v-link="'/users/profile/view/' + currentUser.ID">{{ currentUser.DisplayedName }} ({{ currentUser.VCoins }} vCoins)</a></li>
+			<li><a v-link="'/users/profile/view/' + currentUser.ID">{{ currentUser.DisplayedName }} ({{ currentUser.VActions }} vActions)</a></li>
 			<li><logout-form></logout-form></li>
 		</ul>
 	</bs-navbar>
 </template>
 
-<script type="text/babel">
-import LoginForm from './LoginForm.vue';
-import LogoutForm from './LogoutForm.vue';
-import BsNavbar from '../bootstrap/BsNavbar.vue';
+<script type="text/ecmascript-6">
+	import LoginForm from './LoginForm.vue';
+	import LogoutForm from './LogoutForm.vue';
+	import BsNavbar from '../bootstrap/BsNavbar.vue';
 
-import { usersApi } from '../../utils/resources';
-import { isLogged, isGuest, currentUser } from '../../vuex/getters'
+	import { usersApi } from '../../utils/resources';
+	import { isLogged, isGuest, currentUser } from '../../vuex/getters'
 
-export default {
-	components: {
-		BsNavbar,
-		LoginForm,
-		LogoutForm
-	},
-	vuex: {
-		getters: {
-			isGuest,
-			isLogged,
-			currentUser
+	export default {
+		components: {
+			BsNavbar,
+			LoginForm,
+			LogoutForm
+		},
+		vuex: {
+			getters: {
+				isGuest,
+				isLogged,
+				currentUser
+			}
 		}
-	},
-}
+	}
 </script>
 
 <style>
