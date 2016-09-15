e9765d9c8a466aba5668aaf21d2802c65bc96886
mantal <mantal0@outlook.fr>
2016-09-15 12:11:52 +0200

    Improve TagViewer

--- src/components/requests/TagsViewer.vue
+++ src/components/requests/TagsViewer.vue
@@ -1,14 +1,15 @@
 <template>
 	<div><!-- TODO case insensitve match + don't match selected tags -->
-		<div class="row">
+		<div>
 			<span v-for="tag in parent.Tags" class="tag">
 				{{ tag.Name }} <button v-if="editMode" @click="removeTag($index)" class="btn btn-default tag-delete-button">x</button>
 			</span>
 			<form v-if="editMode" @submit.prevent="addTag" class="form-inline" id="tag-input-form">
-				<input type="text" v-model="newTag" list="tagsList" class="form-control">
+				<input type="text" v-model="newTag" list="tagsList" class="form-control" placeholder="Compétences requises">
 				<datalist id="tagsList">
 					<option v-for="tag in tagsList">{{ tag.Name }}</option>
 				</datalist>
+				<button type="submit" class="btn btn-default">Ajouter</button>
 			</form>
 		</div>
 	</div>
