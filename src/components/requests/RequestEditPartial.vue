fed55756d74d87d2cd6f989d302d69feb3ef4138
mantal <mantal0@outlook.fr>
2016-09-16 13:47:00 +0200

    VON-31 VON-33 Add duration, frequenty and period to requests

--- src/components/requests/RequestEditPartial.vue
+++ src/components/requests/RequestEditPartial.vue
@@ -21,13 +21,58 @@
 				required
 			></textarea>
 		</div>
-		<tags-viewer :parent="request" :edit-mode="true"></tags-viewer>
-		<button type="submit" class="btn btn-primary btn-block btn-lg">Save</button>
+		<div>
+			<tags-viewer :parent="request" :edit-mode="true"></tags-viewer>
+		</div>
+		<div>
+			<div class="form-group">
+				<label for="request-date-start-input">Période souhaitée (début): </label>
+				<input type="date" v-model="request.PeriodStart" id="request-date-start-input" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="request-date-end-input">Période souhaitée (fin): </label>
+				<input type="date" v-model="request.PeriodEnd" id="request-date-end-input" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<div class="form-inline">
+					<div class="form-group">
+						<label for="request-duration-input">Durée </label>
+						<input type="number" v-model="duration" size="2" id="request-duration-input" class="form-control">
+					</div>
+					<div class="form-group">
+						<select v-model="durationUnit" class="form-control">
+							<option value="minute">minutes</option>
+							<option value="hour">heures</option>
+							<option value="day">jours</option>
+						</select>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="form-inline">
+					<div class="form-group">
+						<label for="request-frequency-input">Fréquence</label>
+						<input type="number" v-model="request.Frequency" min="0" id="request-frequency-input" class="form-control"> fois par
+					</div>
+					<div class="form-group">
+						<select v-model="request.FrequencyUnit" class="form-control">
+							<option value="day">jour</option>
+							<option value="week" selected>semaine</option>
+							<option value="month">mois</option>
+						</select>
+					</div>
+				</div>
+			</div>
+
+			<button type="submit" class="btn btn-primary btn-block btn-lg">Save</button>
+		</div>
 	</form>
 </template>
 
 <script type="text/babel">
 	import TagsViewer from './TagsViewer.vue';
+	import moment from 'moment';
 
 	export default {
 		props: {
@@ -39,8 +84,26 @@
 				})
 			}
 		},
+		data() {
+			return {
+				duration: null,
+				durationUnit: 'hour'
+			}
+		},
+		watch: {
+			duration(newValue) {
+				if (newValue)
+					this.request.Duration = moment.duration(Number(this.duration), this.durationUnit).asSeconds();
+			},
+			durationUnit(newValue) {
+				if (newValue)
+					this.request.Duration = moment.duration(Number(this.duration), this.durationUnit).asSeconds();
+			}
+
+		},
 		methods: {
 			save() {
+				this.request.Frequency = Number(this.request.Frequency);
 				this.$dispatch('on-save', this.request);
 			}
 		},
