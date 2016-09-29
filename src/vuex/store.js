481c323daa1a9c2b38c55e2c970137e2d54610db
mantal <mantal0@outlook.fr>
2016-09-29 12:44:59 +0200

    VON-104 Alerts hide after 5 secondes

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -27,6 +27,9 @@ const mutations = {
 	},
 	[A.ALERT] (state, type, message) {
 		state.alerts.push({ type: type, message: message });
+		window.setTimeout(() => {
+			mutations[A.ALERT_DISMISS](state, state.alerts.length - 1);
+		}, 5000);
 	},
 	[A.ALERT_DISMISS] (state, index) {
 		state.alerts.splice(index, index + 1);
